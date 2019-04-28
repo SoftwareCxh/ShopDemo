d2bd1943702316aeed884d60f6584d81e043a57a
SoftwareCxh <13306644202@163.com>
2019-04-28 23:14:47 +0800

    添加登录注册界面

--- app/src/main/java/com/example/graduationproject/ui/RegisterActivity.java
+++ app/src/main/java/com/example/graduationproject/ui/RegisterActivity.java
@@ -3,7 +3,6 @@ package com.example.graduationproject.ui;
 import android.content.Intent;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.example.graduationproject.R;
 import com.example.graduationproject.base.CommActivity;
@@ -11,14 +10,13 @@ import com.example.graduationproject.base.CommActivity;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class LoginActivity extends CommActivity {
-    @BindView(R.id.to_register)
-    LinearLayout ll_toRegister;
-
+public class RegisterActivity extends CommActivity {
+    @BindView(R.id.to_login)
+    LinearLayout ll_toLogin;
 
     @Override
     public int layoutId() {
-        return R.layout.activity_login;
+        return R.layout.activity_register;
     }
 
     @Override
@@ -26,17 +24,19 @@ public class LoginActivity extends CommActivity {
 
     }
 
-    @OnClick({R.id.to_register})
+
+    @OnClick({R.id.to_login})
     public void onClick(View v){
         switch (v.getId()){
-            case R.id.to_register:
-                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+            case R.id.to_login:
+                Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
                 startActivity(intent);
                 finish();
                 break;
 
         }
     }
+
 
     @Override
     public void initPresenter() {
--- app/src/main/java/com/example/graduationproject/ui/mine/MineFragment.java
+++ app/src/main/java/com/example/graduationproject/ui/mine/MineFragment.java
@@ -1,13 +1,23 @@
 package com.example.graduationproject.ui.mine;
 
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.graduationproject.R;
 import com.example.graduationproject.base.CommFragment;
+import com.example.graduationproject.ui.LoginActivity;
+import com.youth.banner.loader.ImageLoader;
+
+import butterknife.BindView;
 
 public class MineFragment extends CommFragment {
+    @BindView(R.id.head_iv)
+    ImageView iv_head;
+
     public static MineFragment getMineView(){
         MineFragment mineFragment=new MineFragment();
         return mineFragment;
@@ -20,7 +30,14 @@ public class MineFragment extends CommFragment {
 
     @Override
     protected void initView(LayoutInflater inflater, @Nullable ViewGroup container) {
-
+        iv_head.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
