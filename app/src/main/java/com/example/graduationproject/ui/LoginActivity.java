f6875fbaccee4a7d82920dd90453211f4eadab56
SoftwareCxh <13306644202@163.com>
2019-04-28 23:39:02 +0800

    添加了登录注册界面逻辑

--- app/src/main/java/com/example/graduationproject/ui/LoginActivity.java
+++ app/src/main/java/com/example/graduationproject/ui/LoginActivity.java
@@ -2,11 +2,13 @@ package com.example.graduationproject.ui;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.graduationproject.R;
 import com.example.graduationproject.base.CommActivity;
+import com.example.graduationproject.util.ToastUtils;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -14,6 +16,12 @@ import butterknife.OnClick;
 public class LoginActivity extends CommActivity {
     @BindView(R.id.to_register)
     LinearLayout ll_toRegister;
+    @BindView(R.id.username)
+    EditText et_username;
+    @BindView(R.id.password)
+    EditText et_password;
+    @BindView(R.id.login)
+    TextView tv_login;
 
 
     @Override
@@ -26,7 +34,7 @@ public class LoginActivity extends CommActivity {
 
     }
 
-    @OnClick({R.id.to_register})
+    @OnClick({R.id.to_register,R.id.login})
     public void onClick(View v){
         switch (v.getId()){
             case R.id.to_register:
@@ -34,8 +42,19 @@ public class LoginActivity extends CommActivity {
                 startActivity(intent);
                 finish();
                 break;
+            case R.id.login:
+                String username=et_username.getText().toString().trim();
+                String password=et_password.getText().toString().trim();
+                if("".equals(username)||"".equals(password)){
+                    ToastUtils.showShort(getBaseContext(),"用户名或密码不能为空");
+                }else{
+                    /**
+                     * 发起网络请求
+                     */
+                }
 
         }
+
     }
 
     @Override
--- app/src/main/java/com/example/graduationproject/ui/RegisterActivity.java
+++ app/src/main/java/com/example/graduationproject/ui/RegisterActivity.java
@@ -1,8 +1,14 @@
 package com.example.graduationproject.ui;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.graduationproject.R;
 import com.example.graduationproject.base.CommActivity;
@@ -13,6 +19,27 @@ import butterknife.OnClick;
 public class RegisterActivity extends CommActivity {
     @BindView(R.id.to_login)
     LinearLayout ll_toLogin;
+    @BindView(R.id.username)
+    EditText et_username;
+    @BindView(R.id.password)
+    EditText et_password;
+    @BindView(R.id.name)
+    EditText et_name;
+    @BindView(R.id.telephone)
+    EditText et_telephone;
+    @BindView(R.id.register)
+    TextView tv_register;
+
+    @BindView(R.id.username_root)
+    TextInputLayout usernameInputLayout;
+    @BindView(R.id.password_root)
+    TextInputLayout passwordInputLayout;
+    @BindView(R.id.name_root)
+    TextInputLayout nameInputLayout;
+    @BindView(R.id.telephone_root)
+    TextInputLayout telephoneInputLayout;
+
+
 
     @Override
     public int layoutId() {
@@ -21,11 +48,97 @@ public class RegisterActivity extends CommActivity {
 
     @Override
     public void initView() {
+        et_username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().length()==0){
+                    usernameInputLayout.setError("账号不能为空");
+                    usernameInputLayout.setErrorEnabled(true);
+                }else{
+                    usernameInputLayout.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        et_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().length()==0){
+                    passwordInputLayout.setError("密码不能为空");
+                    passwordInputLayout.setErrorEnabled(true);
+                }else {
+                    passwordInputLayout.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        et_name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().length()==0){
+                    nameInputLayout.setError("用户名不能为空");
+                    nameInputLayout.setErrorEnabled(true);
+                }else{
+                    nameInputLayout.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        et_telephone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().length()==11){
+                    telephoneInputLayout.setErrorEnabled(false);
+                }else{
+                    telephoneInputLayout.setError("手机号格式错误");
+                    telephoneInputLayout.setErrorEnabled(true);
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
 
-    @OnClick({R.id.to_login})
+    @OnClick({R.id.to_login,R.id.register})
     public void onClick(View v){
         switch (v.getId()){
             case R.id.to_login:
@@ -33,6 +146,22 @@ public class RegisterActivity extends CommActivity {
                 startActivity(intent);
                 finish();
                 break;
+            case R.id.register:
+                if(telephoneInputLayout.isErrorEnabled()||
+                        nameInputLayout.isErrorEnabled()||
+                        usernameInputLayout.isErrorEnabled()||
+                        passwordInputLayout.isErrorEnabled()){
+                    Toast.makeText(RegisterActivity.this,"请完善个人信息",Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    String username=et_username.getText().toString();
+                    String password=et_password.getText().toString();
+                    String name=et_name.getText().toString();
+                    String telephone=et_telephone.getText().toString();
+                    /**
+                     * 发起注册的网络请求
+                     */
+                }
 
         }
     }
